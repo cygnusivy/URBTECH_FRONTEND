6b88620760885e70bbb0362c8c907641a9a9a276
Nascimento <nascimentoeverton568@gmail.com>
2023-04-06 23:50:10 -0300

    Adicionando mapa de locações

--- home/script.js
+++ home/script.js
@@ -1,45 +1,69 @@
-/*var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
-*/
 var map;
+var opcao = "estacoes.json";
+var markers;
 
-fetch("estacoes.json").then((response) =>
-{
-    response.json().then((teste) =>
-    {
-       map = L.map('map').setView([teste.records[0][4], teste.records[0][5]], 13);
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
+function fetchMapData(opcao) {
+  if (!map) {
+    fetch(opcao)
+      .then((response) => response.json())
+      .then((teste) => {
+        map = L.map("map").setView(
+          [teste.records[0][4], teste.records[0][5]],
+          13
+        );
 
-        teste.records.map((record) => 
-        {
-            var mark = L.marker(
-                L.latLng(
-                  parseFloat([record[4]]),
-                  parseFloat([record[5]])
-                )
-              )
-              .addTo(map)
-              .bindPopup(record[2])
-        })
-    })
-})
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
 
-function op(c){ 
-    var item = document.getElementById('item-'+ c).innerHTML; 
-    document.getElementsByTagName('input')[0].value = item
+        markers = L.layerGroup().addTo(map);
+
+        teste.records.forEach((record) => {
+          var mark = L.marker(
+            L.latLng(parseFloat([record[4]]), parseFloat([record[5]]))
+          )
+            .addTo(markers)
+            .bindPopup(record[2]);
+        });
+      });
+  } else {
+    fetch(opcao)
+      .then((response) => response.json())
+      .then((teste) => {
+        markers.clearLayers();
+
+        teste.records.forEach((record) => {
+          var mark = L.marker(
+            L.latLng(parseFloat([record[4]]), parseFloat([record[5]]))
+          ).bindPopup(record[2]);
+
+          markers.addLayer(mark);
+        });
+      });
+  }
 }
-function dropdown(p){
-    var e = document.getElementsByClassName('dropdown')[0];
-    var d = ['block','none'];
 
-    e.style.display = d[p]
+fetchMapData(opcao);
+
+function op(c) {
+  var item = document.getElementById("item-" + c).innerHTML;
+  document.getElementsByName("opcoes")[0].value = item;
+  if (item === "Estacoes de locacao") {
+    opcao = "locacoes.json";
+  } else if (item === "Estacoes de Reparo") {
+    opcao = "estacoes.json";
+  } else if (item === "Rotas seguras") {
+    opcao = "estacoes.json";
+  } else {
+    opcao = "estacoes.json";
+  }
+  fetchMapData(opcao);
+}
+
+function dropdown(p) {
+  var e = document.getElementsByClassName("dropdown")[0];
+  var d = ["block", "none"];
+
+  e.style.display = d[p];
 }
