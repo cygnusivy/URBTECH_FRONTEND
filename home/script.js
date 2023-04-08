1e1a588097e8f059f80bc1ab1ba34929247ff11f
Nascimento <nascimentoeverton568@gmail.com>
2023-04-08 18:52:12 -0300

    ajustes

--- home/script.js
+++ home/script.js
@@ -1,3 +1,13 @@
+/*var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+*/
 var map;
 var opcao = "estacoes.json";
 var markers;
@@ -61,9 +71,51 @@ function op(c) {
   fetchMapData(opcao);
 }
 
-function dropdown(p) {
-  var e = document.getElementsByClassName("dropdown")[0];
-  var d = ["block", "none"];
+function dropdown(p){
+    var e = document.getElementsByClassName('dropdown')[0];
+    var d = ['block','none'];
 
-  e.style.display = d[p];
+    e.style.display = d[p]
 }
+
+function setTopo(){
+    $(window).scrollTop(0);
+}
+$(window).bind('scroll', setTopo);
+
+function opMob(c){ 
+    var item = document.getElementById('item-'+ c).innerHTML; 
+    document.getElementById('input-mobile').value = item;
+}
+function dropdownMob(p){
+    var e = document.getElementsByClassName('dropdown-mobile')[0];
+    var d = ['block','none'];
+
+    e.style.display = d[p]
+}
+
+var big = true;
+
+$('#inputmobile').click(function() {
+    if(big) {
+      big = false;
+      $('.barrainferior').css('top', '74vh');
+      dropdownMob(1);
+    } else {
+      big = true;
+      $('.barrainferior').css('top', '64vh');
+      dropdownMob(0);
+    }
+  });
+
+$('.divider').click(function() {
+  if(big) {
+    big = false;
+    $('.barrainferior').css('top', '74vh');
+    dropdownMob(1);
+  } else {
+    big = true;
+    $('.barrainferior').css('top', '64vh');
+    dropdownMob(0);
+  }
+});
