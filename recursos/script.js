9e4ce188b3c3b0f1bd9d44ade3e2f5eca9e47555
Nascimento <nascimentoeverton568@gmail.com>
2023-04-12 15:39:14 -0300

    adicionando recursos

---
+++ recursos/script.js
@@ -0,0 +1,175 @@
+/*var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+*/
+var map;
+var opcao = "estacoes.json";
+var markers;
+var target = document.getElementById('target');
+var watchId;
+
+var userMarker;
+
+// ...
+
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+
+  if (userMarker) {
+    map.removeLayer(userMarker);
+  }
+
+  userMarker = L.marker([location.coords.latitude, location.coords.longitude]).addTo(map);
+
+  userMarker.setLatLng([location.coords.latitude, location.coords.longitude]);
+}
+
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+  target.innerText = 'Geolocation API not supported.';
+}
+
+navigator.geolocation.getCurrentPosition(function (location) {
+  appendLocation(location, 'fetched');
+
+  if (!userMarker) {
+    userMarker = L.marker([location.coords.latitude, location.coords.longitude]).addTo(map);
+  } else {
+    userMarker.setLatLng([location.coords.latitude, location.coords.longitude]);
+  }
+  map.setView([location.coords.latitude, location.coords.longitude], 13);
+});
+
+function fetchMapData(opcao) {
+  if (!map) {
+    fetch(opcao)
+      .then((response) => response.json())
+      .then((teste) => {
+        map = L.map("map").setView(
+          [teste.records[0][4], teste.records[0][5]],
+          13
+        );
+
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
+
+        markers = L.layerGroup().addTo(map);
+
+        teste.records.forEach((record) => {
+          var mark = L.marker(
+            L.latLng(parseFloat([record[4]]), parseFloat([record[5]]))
+          )
+            .addTo(markers)
+            .bindPopup(record[2]);
+        });
+      });
+  } else {
+    fetch(opcao)
+      .then((response) => response.json())
+      .then((teste) => {
+        markers.clearLayers();
+
+        teste.records.forEach((record) => {
+          var mark = L.marker(
+            L.latLng(parseFloat([record[4]]), parseFloat([record[5]]))
+          ).bindPopup(record[2]);
+
+          markers.addLayer(mark);
+        });
+      });
+  }
+}
+
+fetchMapData(opcao);
+
+function op(c) {
+  var item = document.getElementById("item-" + c).innerHTML;
+  document.getElementsByName("opcoes")[0].value = item;
+  if (item === "Estacoes de locacao") {
+    opcao = "locacoes.json";
+  } else if (item === "Estacoes de Reparo") {
+    opcao = "estacoes.json";
+  } else if (item === "Acidentes") {
+    opcao = "acidentes.json";
+  } else {
+    opcao = "estacoes.json";
+  }
+  fetchMapData(opcao);
+}
+
+function dropdown(p){
+    var e = document.getElementsByClassName('dropdown')[0];
+    var d = ['block','none'];
+
+    e.style.display = d[p]
+}
+
+function setTopo(){
+    $(window).scrollTop(0);
+}
+$(window).bind('scroll', setTopo);
+
+function opMob(c){
+  var item = document.getElementById("item-" + c).innerHTML;
+  document.getElementsByName("opcoes")[0].value = item;
+  if (item === "Estacoes de locacao") {
+    opcao = "locacoes.json";
+  } else if (item === "Estacoes de Reparo") {
+    opcao = "estacoes.json";
+  } else if (item === "Acidentes") {
+    opcao = "acidentes.json";
+  } else {
+    opcao = "estacoes.json";
+  }
+  fetchMapData(opcao);
+}
+
+function dropdownMob(p){
+    var e = document.getElementsByClassName('dropdown-mobile')[0];
+    var d = ['block','none'];
+
+    e.style.display = d[p]
+}
+
+var big = true;
+
+$('#inputmobile').click(function() {
+    if(big) {
+      big = false;
+      $('.barrainferior').css('top', '74vh');
+      dropdownMob(1);
+    } else {
+      big = true;
+      $('.barrainferior').css('top', '64vh');
+      dropdownMob(0);
+    }
+  });
+
+$('.divider').click(function() {
+  if(big) {
+    big = false;
+    $('.barrainferior').css('top', '74vh');
+    dropdownMob(1);
+  } else {
+    big = true;
+    $('.barrainferior').css('top', '64vh');
+    dropdownMob(0);
+  }
+});
